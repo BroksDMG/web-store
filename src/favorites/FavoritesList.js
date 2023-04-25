2b88dcd7930980304e3a4322b135e0d64334b5ad
Kuba_s <86146547+BroksDMG@users.noreply.github.com>
2023-04-25 23:49:42 +0200

    card in favorites

--- src/favorites/FavoritesList.js
+++ src/favorites/FavoritesList.js
@@ -3,14 +3,26 @@ import NavBar from '../navbar/NavBar'
 function FavoritesList() {
   return (
     <div>
-        <NavBar/>
-    <div className='max-w-[1440px]'>
-        <div className='w-screen '>
-        Favorites
+    <NavBar></NavBar>
+    <div className="max-w-[1440px]  h-screen flex ">
+      <div className='w-screen flex bg-black'>
+        <div className=" bg-green-300 p-3 w-full">
+          <button className="z-40 cursor-pointer text-3xl relative left-7 top-7">
+
+          </button>
+          
+        </div>
+        <div className=" bg-green-300 p-3">
+          <button className="z-40 cursor-pointer text-3xl relative left-7 top-7">
+
+          </button>
+          
+        </div>
+        <div className=" font-robo font-bold tracking-wider p-3 relative flex-grow flex flex-col justify-between bg-gray-100 h-full">
 
         </div>
-
-    </div>
+      </div>
+      </div>
     </div>
   )
 }
