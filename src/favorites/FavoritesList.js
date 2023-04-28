74871d41fdb6134e9d7243883f10374618d74cfb
Kuba_s <86146547+BroksDMG@users.noreply.github.com>
2023-04-28 18:48:10 +0200

    changes in favoritescard

--- src/favorites/FavoritesList.js
+++ src/favorites/FavoritesList.js
@@ -13,10 +13,7 @@ function FavoritesList() {
           
         </div>
         <div className=" bg-green-300 p-3">
-          <button className="z-40 cursor-pointer text-3xl relative left-7 top-7">
-
-          </button>
-          
+        
         </div>
         <div className=" font-robo font-bold tracking-wider p-3 relative flex-grow flex flex-col justify-between bg-gray-100 h-full">
 
